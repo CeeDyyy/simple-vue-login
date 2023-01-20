3ed18ccd786fb48d71183b2c0ff6b48aac4435cd
CeeDyyy <thanawat.ud@mail.kmutt.ac.th>
2023-01-20 12:57:18 +0700

    update login form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,53 @@
 <template>
   <div class="login">
-    <h1>This is an login page</h1>
+    <h1>Login</h1>
+    <form @submit.prevent="loginSubmit">
+      <div class="form-control">
+        <label for="username">Username</label>
+        <input type="text" name="username" placeholder="Username" />
+      </div>
+      <div class="form-control">
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="Password" />
+      </div>
+      <button type="submit">Login</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "LoginView",
+  data() {
+    return {
+      input: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    loginSubmit(loginEvent) {
+      if (
+        loginEvent.target.elements.username.value != "" &&
+        loginEvent.target.elements.password.value != ""
+      ) {
+        this.username = loginEvent.target.elements.username.value;
+        this.password = loginEvent.target.elements.password.value;
+        console.log("login");
+      } else {
+        console.log("please enter username and password");
+      }
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.form-control {
+    padding-bottom: 10px;
+}
+.form-control label {
+    padding-right: 10px;
+}
 </style>
