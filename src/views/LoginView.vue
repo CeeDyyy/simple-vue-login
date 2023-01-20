1da68289c753ebeaa723a9eb25f0a471a86d0085
CeeDyyy <thanawat.ud@mail.kmutt.ac.th>
2023-01-20 17:59:43 +0700

    use v-model instead of form and use axios for get data from api and some of show data after login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,53 +1,57 @@
 <template>
   <div class="login">
     <h1>Login</h1>
-    <form @submit.prevent="loginSubmit">
-      <div class="form-control">
-        <label for="username">Username</label>
-        <input type="text" name="username" placeholder="Username" />
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" name="password" placeholder="Password" />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+    <div class="form-control">
+      <label for="username">Username</label>
+      <input type="text" name="username" v-model.trim="username" placeholder="Username" />
+    </div>
+    <div class="form-control">
+      <label for="password">Password</label>
+      <input type="password" name="password" v-model.trim="password" placeholder="Password" />
+    </div>
+    <button type="button" @click="login()">Login</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: "LoginView",
   data() {
     return {
-      input: {
-        username: "",
-        password: "",
-      },
+      username: "",
+      password: "",
     };
   },
   methods: {
-    loginSubmit(loginEvent) {
-      if (
-        loginEvent.target.elements.username.value != "" &&
-        loginEvent.target.elements.password.value != ""
-      ) {
-        this.username = loginEvent.target.elements.username.value;
-        this.password = loginEvent.target.elements.password.value;
-        console.log("login");
+    async login() {
+      if (this.username != "" && this.password != "") {
+        let result = await axios.get(
+          `http://localhost:3000/users?username=${this.username}&password=${this.password}`
+        );
+        if(result.status == 200 && result.data.length > 0) {
+          localStorage.setItem("user-info", JSON.stringify(result.data[0]))
+          this.$router.push({name: 'home'})
+        }
       } else {
         console.log("please enter username and password");
       }
     },
   },
+  mounted() {
+      let user = localStorage.getItem('user-info');
+      if(user) {
+        this.$router.push({name: 'home'})
+      }
+    }
 };
 </script>
 
 <style scoped>
 .form-control {
-    padding-bottom: 10px;
+  padding-bottom: 10px;
 }
 .form-control label {
-    padding-right: 10px;
+  padding-right: 10px;
 }
 </style>
